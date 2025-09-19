b062868276a42d6f14ed7d95249b3196b5ead595
jomuch <52454755+jomuch@users.noreply.github.com>
2025-09-19 09:57:58 -0400

    Updating with CRUD

--- Pages/Departments/Index.cshtml.cs
+++ Pages/Departments/Index.cshtml.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using ContosoUniversity.Data;
 using ContosoUniversity.Models;
 
-namespace ContosoUniversity.Pages_Departments
+namespace ContosoUniversity.Pages.Departments
 {
     public class IndexModel : PageModel
     {
@@ -19,12 +19,52 @@ namespace ContosoUniversity.Pages_Departments
             _context = context;
         }
 
-        public IList<Department> Department { get;set; } = default!;
+        public IList<Department> Department { get; set; } = default!;
+        public string NameSort { get; set; } = null!;
+        public string BudgetSort { get; set; } = null!;
+        public string StartDateSort { get; set; } = null!;
+        public string InstructorSort { get; set; } = null!;
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
-            Department = await _context.Departments
-                .Include(d => d.Administrator).ToListAsync();
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            BudgetSort = sortOrder == "Budget" ? "budget_desc" : "Budget";
+            StartDateSort = sortOrder == "StartDate" ? "startdate_desc" : "StartDate";
+            InstructorSort = sortOrder == "Instructor" ? "instructor_desc" : "Instructor";
+
+            IQueryable<Department> departments = _context.Departments
+                .Include(d => d.Administrator)
+                .AsNoTracking();
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    departments = departments.OrderByDescending(d => d.Name);
+                    break;
+                case "Budget":
+                    departments = departments.OrderBy(d => d.Budget);
+                    break;
+                case "budget_desc":
+                    departments = departments.OrderByDescending(d => d.Budget);
+                    break;
+                case "StartDate":
+                    departments = departments.OrderBy(d => d.StartDate);
+                    break;
+                case "startdate_desc":
+                    departments = departments.OrderByDescending(d => d.StartDate);
+                    break;
+                case "Instructor":
+                    departments = departments.OrderBy(d => d.Administrator.LastName);
+                    break;
+                case "instructor_desc":
+                    departments = departments.OrderByDescending(d => d.Administrator.LastName);
+                    break;
+                default:
+                    departments = departments.OrderBy(d => d.Name);
+                    break;
+            }
+
+            Department = await departments.ToListAsync();
         }
     }
 }
--- Pages/Instructors/Index.cshtml.cs
+++ Pages/Instructors/Index.cshtml.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using ContosoUniversity.Data;
 using ContosoUniversity.Models;
 
-namespace ContosoUniversity.Pages_Instructors
+namespace ContosoUniversity.Pages.Instructors
 {
     public class IndexModel : PageModel
     {
@@ -19,11 +19,68 @@ namespace ContosoUniversity.Pages_Instructors
             _context = context;
         }
 
-        public IList<Instructor> Instructor { get;set; } = default!;
+        public PaginatedList<Instructor> Instructor { get; set; }
+        public string NameSort { get; set; } = null!;
+        public string DateSort { get; set; } = null!;
+        public string CurrentFilter { get; set; } = null!;
+        public string CurrentSort { get; set; } = null!;
+        public Instructor InstructorData { get; set; } = new Instructor();
+        public ICollection<Course> AssignedCoursesData { get; set; } = new List<Course>();
+        public int InstructorID { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex, int? id)
         {
-            Instructor = await _context.Instructors.ToListAsync();
+            CurrentSort = sortOrder;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            CurrentFilter = searchString;
+
+            IQueryable<Instructor> instructorsIQ = from i in _context.Instructors
+                .Include(i => i.OfficeAssignment)
+                .Include(i => i.CourseAssignments)
+                .ThenInclude(i => i.Course)
+                                                   select i;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                instructorsIQ = instructorsIQ.Where(i => i.LastName.Contains(searchString) || i.FirstMidName.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    instructorsIQ = instructorsIQ.OrderByDescending(i => i.LastName);
+                    break;
+                case "Date":
+                    instructorsIQ = instructorsIQ.OrderBy(i => i.HireDate);
+                    break;
+                case "date_desc":
+                    instructorsIQ = instructorsIQ.OrderByDescending(i => i.HireDate);
+                    break;
+                default:
+                    instructorsIQ = instructorsIQ.OrderBy(i => i.LastName);
+                    break;
+            }
+
+            int pageSize = 3;
+            Instructor = await PaginatedList<Instructor>.CreateAsync(instructorsIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
+
+            if (id != null)
+            {
+                InstructorID = id.Value;
+                InstructorData = Instructor.Single(i => i.ID == id.Value);
+                var courses = InstructorData.CourseAssignments.Select(s => s.Course);
+                AssignedCoursesData = await courses.ToListAsync();
+            }
         }
     }
 }
