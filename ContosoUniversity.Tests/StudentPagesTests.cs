e6d98fc06fb203062d6f509c3a7ba857522e3b28
jomuch <52454755+jomuch@users.noreply.github.com>
2025-10-04 10:47:53 -0400

    Fixing installed programs

--- ContosoUniversity.Tests/StudentPagesTests.cs
+++ ContosoUniversity.Tests/StudentPagesTests.cs
@@ -1,36 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
-using System.Net;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net; 
+using System.Threading.Tasks; 
 using Xunit;
 
-namespace ContosoUniversity.Tests
-{
-    public class StudentPagesTests : IClassFixture<WebApplicationFactory<Program>>
-    {
+namespace ContosoUniversity.Tests {
+    public class StudentPagesTests : IClassFixture<WebApplicationFactory<Program>> {
         private readonly WebApplicationFactory<Program> _factory;
-
-        public StudentPagesTests(WebApplicationFactory<Program> factory)
-        {
-            _factory = factory;
-        }
-
+        public StudentPagesTests(WebApplicationFactory<Program> factory) { _factory = factory; }
         [Fact]
-        public async Task Get_StudentsIndexPage_ReturnsSuccessAndCorrectContentType()
-        {
+        public async Task Get_StudentsIndexPage_ReturnsSuccessAndCorrectContentType() {
             var client = _factory.CreateClient();
             var response = await client.GetAsync("/Students");
-            response.EnsureSuccessStatusCode();
-            Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
+            response.EnsureSuccessStatusCode(); Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
         }
-
         [Fact]
-        public async Task Get_StudentsDetailsPage_ReturnsNotFoundForInvalidId()
-        {
+        public async Task Get_StudentsDetailsPage_ReturnsNotFoundForInvalidId() {
             var client = _factory.CreateClient();
             var response = await client.GetAsync("/Students/Details?id=999");
             Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
         }
     }
 }
-
