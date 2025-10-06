700f785110129b7aa8ff61a99c77b86c3780ceae
jomuch <52454755+jomuch@users.noreply.github.com>
2025-10-06 09:08:43 -0400

    update

---
+++ Migrations/20251006130523_TestsCourses.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContosoUniversity.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestsCourses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirstMidName",
+                table: "Students",
+                newName: "FirstName");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstMidName",
+                table: "Instructors",
+                newName: "FirstName");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "OfficeAssignments",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Students",
+                newName: "FirstMidName");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Instructors",
+                newName: "FirstMidName");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "OfficeAssignments",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
